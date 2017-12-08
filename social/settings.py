741abcf2716be3c29f9af6833b7f84aa11687462
Vitaly <vs3rgienko@gmail.com>
2017-12-08 08:49:37 +0200

    omg

--- social/settings.py
+++ social/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/post_images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'post_images')
 
 MEDIA_URL = '/'
 
